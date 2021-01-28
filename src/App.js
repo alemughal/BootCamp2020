c0bf2151fa6b239fd8ed07bd957254424332fb53
Ali Mughal <alimug38@gmail.com>
2021-01-28 12:22:59 +0500

    Bar Chart using Chart.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './NavBar';
 import LogIn from './Login';
+import Bar from './Bar';
 
 function App() {
   return (
     <div>
       <NavBar />
       <LogIn />
+      <Bar />
     </div>
   );
 }
